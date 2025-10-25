7607f4ae7b3f092e0cefb8c20e34f4ec21d119d2
Muciu <constantinmucioniu@gmail.com>
2025-10-25 18:11:03 +0300

    test to check cloudflare pipeline

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || "No PORT found in .env file";
 
+// TEST
+
 if (PORT === "No PORT found in .env file") {
   console.error(PORT);
   process.exit(1);
