f419e98703cd860ddf16dd9fd836c79b0c8dacf1
Muciu <constantinmucioniu@gmail.com>
2025-10-25 18:02:51 +0300

    set-up for node in backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || "No PORT found in .env file";
+
+if (PORT === "No PORT found in .env file") {
+  console.error(PORT);
+  process.exit(1);
+}
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
